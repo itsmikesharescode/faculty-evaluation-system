7d15f407c00a6d8fe7707258c9854678b232060e
itsmikesharescode <mikejohneviota@gmail.com>
2024-08-08 21:54:09 +0800

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<div class="flex min-h-screen flex-col items-center justify-center">
+	<p>On-progress Faculty Evaluation System <strong class="underline">One Cainta College</strong></p>
+</div>
