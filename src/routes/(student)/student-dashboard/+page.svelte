664667879414eca43fb98eb4f5fe55ffafeabe71
itsmikesharescode <mikejohneviota@gmail.com>
2024-08-09 15:39:53 +0800

    ez front end

--- src/routes/(student)/student-dashboard/+page.svelte
+++ src/routes/(student)/student-dashboard/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
 	import { BellRing } from 'lucide-svelte';
 </script>
 
 <div class="min-h-screen">
 	<div class="md:p-[1.25rem]] bg-slate-100 p-[0.625rem]">
-		<div class="flex items-center gap-[20px] bg-green-500 p-[10px]">
+		<div class="flex flex-wrap items-center gap-[20px] bg-green-500 p-[10px] md:flex-nowrap">
 			<BellRing class="h-full w-[60px] bg-[#89CFF0] p-[10px] text-white" />
 			<div class="text-white">
 				<p class="font-semibold leading-7">Reminder</p>
@@ -25,7 +26,9 @@
 			</p>
 		</div>
 
-		<div class="grid grid-cols-[5%,40%,30%,15%,10%] border-t-[1px] border-slate-400 bg-slate-100">
+		<div
+			class="sticky top-[3rem] hidden border-t-[1px] border-slate-400 bg-slate-100 md:grid md:grid-cols-[5%,25%,25%,25%,20%] lg:grid-cols-[5%,40%,30%,15%,10%]"
+		>
 			<span
 				class="flex items-center justify-center border-r-[1px] border-slate-400 p-[10px] text-sm font-semibold"
 				>#</span
@@ -37,33 +40,57 @@
 		</div>
 
 		<!--Looping table here-->
-		{#each Array(30) as _, index}
-			<div class="grid grid-cols-[5%,40%,30%,15%,10%] border-t-[1px] border-slate-400 bg-slate-300">
-				<span
-					class="flex items-center justify-center border-r-[1px] border-slate-400 p-[10px] text-sm font-semibold"
-					>{index + 1}</span
+		<div class="mt-[2dvh] flex flex-col gap-[10px] md:mt-0 md:gap-0">
+			{#each Array(40) as _, index}
+				<div
+					class="hidden border-t-[1px] border-slate-400 bg-slate-300 md:grid md:grid-cols-[5%,25%,25%,25%,20%] lg:grid-cols-[5%,40%,30%,15%,10%]"
 				>
-				<span
-					class="flex items-center border-r-[1px] border-slate-400 p-[10px] text-sm font-semibold"
-					>Faculty Test {index + 1}</span
-				>
-				<span
-					class="flex items-center border-r-[1px] border-slate-400 p-[10px] text-sm font-semibold"
-					>COMSCI 1110</span
-				>
-				<span
-					class="flex items-center border-r-[1px] border-slate-400 p-[10px] text-sm font-semibold"
-				>
-					<div class="flex max-w-fit items-center bg-orange-500 p-[10px] text-xs text-white">
-						NOT STARTED
+					<span
+						class="flex items-center justify-center border-r-[1px] border-slate-400 p-[10px] text-sm font-semibold"
+						>{index + 1}</span
+					>
+					<span
+						class="flex items-center border-r-[1px] border-slate-400 p-[10px] text-sm font-semibold"
+						>Faculty Test {index + 1}</span
+					>
+					<span
+						class="flex items-center border-r-[1px] border-slate-400 p-[10px] text-sm font-semibold"
+						>COMSCI 1110</span
+					>
+					<span
+						class="flex items-center border-r-[1px] border-slate-400 p-[10px] text-sm font-semibold"
+					>
+						<div class="flex max-w-fit items-center bg-orange-500 p-[10px] text-xs text-white">
+							NOT STARTED
+						</div>
+					</span>
+					<span class="flex items-center p-[10px] text-sm font-semibold">
+						<button class="flex max-w-fit items-center bg-green-500 p-[10px] text-xs text-white">
+							EVALUATE
+						</button>
+					</span>
+				</div>
+
+				<!--Render for mobile view-->
+				<div class="md:hidden">
+					<div class="relative flex flex-col gap-[20px] bg-slate-300 p-[10px]">
+						<p>#: {index + 1}</p>
+
+						<div
+							class="absolute right-0 top-0 m-[10px] flex max-w-fit items-center bg-orange-500 p-[10px] text-xs text-white"
+						>
+							NOT STARTED
+						</div>
+
+						<p>Teacher: Faculty Text {index + 1}</p>
+						<p>Subject: COMSCI 1110</p>
+
+						<button class="bg-green-500 p-[10px] text-xs font-semibold text-white"
+							>Evaluate Now</button
+						>
 					</div>
-				</span>
-				<span class="flex items-center p-[10px] text-sm font-semibold">
-					<div class="flex max-w-fit items-center bg-green-500 p-[10px] text-xs text-white">
-						EVALUATE
-					</div>
-				</span>
-			</div>
-		{/each}
+				</div>
+			{/each}
+		</div>
 	</div>
 </div>
