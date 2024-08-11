670d7dd5776171c6b23def69ac711789a4e1d118
itsmikesharescode <mikejohneviota@gmail.com>
2024-08-11 23:00:29 +0800

    add supabase config

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
 	import '../app.css';
 	import '@fontsource/poppins';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
 
-	const { children } = $props();
+	const { data, children } = $props();
+
+	onMount(() => {
+		const { session, supabase } = data;
+
+		const { data: subs } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => subs.subscription.unsubscribe();
+	});
 </script>
 
 {@render children()}
