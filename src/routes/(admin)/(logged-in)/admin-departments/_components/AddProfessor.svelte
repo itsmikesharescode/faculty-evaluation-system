2e4af208bae595a40230ba0dbfb1088866a97aa1
itsmikesharescode <mikejohneviota@gmail.com>
2024-08-27 22:53:44 +0800

    update UX guides

--- src/routes/(admin)/(logged-in)/admin-departments/_components/AddProfessor.svelte
+++ src/routes/(admin)/(logged-in)/admin-departments/_components/AddProfessor.svelte
@@ -108,13 +108,13 @@
 				</Popover.Trigger>
 				<Popover.Content>
 					<p class="text-sm leading-7">
-						Please use formats like <strong>24BSIS-1M, 24BSIS-2M, 23BSIS-2P1E</strong>.
+						Please use formats like <strong>24BSIS-1M,24BSIS-2M,23BSIS-2P1E</strong>
 					</p>
 
 					<p class="text-sm leading-7">Example:</p>
 					<p class="text-sm leading-7">Single section: <strong>24BSIS-1M</strong></p>
 					<p class="text-sm leading-7">
-						Multiple section: <strong>24BSIS-1M, 24BSIS-2M, 23BSIS-2P1E</strong>.
+						Multiple section: <strong>24BSIS-1M,24BSIS-2M,23BSIS-2P1E</strong>
 					</p>
 				</Popover.Content>
 			</Popover.Root>
