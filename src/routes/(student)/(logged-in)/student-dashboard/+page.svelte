a7077b7ccc8c3a13edf4f4f79b0d8a227863bedc
itsmikesharescode <mikejohneviota@gmail.com>
2024-08-29 16:28:12 +0800

    update table for future pagination

--- src/routes/(student)/(logged-in)/student-dashboard/+page.svelte
+++ src/routes/(student)/(logged-in)/student-dashboard/+page.svelte
@@ -6,6 +6,7 @@
 	import type { ProfessorType } from '$lib/types';
 	import { goto } from '$app/navigation';
 	import * as Table from '$lib/components/ui/table';
+	import Actions from './_components/Actions.svelte';
 
 	const route = fromStudentRouteState();
 	const dashboardRoute = fromDashboardRouteState();
@@ -43,7 +44,6 @@
 		</div>
 
 		<Table.Root>
-			<Table.Caption>A list of your recent invoices.</Table.Caption>
 			<Table.Header>
 				<Table.Row>
 					<Table.Head class="truncate">Teacher Name</Table.Head>
@@ -79,42 +79,33 @@
 			<p class="text-muted-foreground">Below are the record of evaluation.</p>
 		</div>
 
-		<div
-			class="sticky top-[3rem] hidden bg-secondary md:grid md:grid-cols-[50%,25%,25%] lg:grid-cols-[55%,30%,15%]"
-		>
-			<span class=" p-[10px] text-sm font-semibold">Teacher</span>
-			<span class=" p-[10px] text-sm font-semibold">Department</span>
-			<span class=" p-[10px] text-sm font-semibold">Result</span>
-		</div>
-
-		<!--Looping table here-->
-		<div class="mt-[2dvh] flex flex-col gap-[10px] md:mt-0 md:gap-0">
-			{#each dashboardRoute.getEvalds() ?? [] as professor, index}
-				<div
-					class="hidden border-b-[1px] md:grid md:grid-cols-[50%,25%,25%] lg:grid-cols-[55%,30%,15%]"
-				>
-					<span class="flex items-center p-[10px] text-sm">{professor.fullname}</span>
-					<span class="flex items-center p-[10px] text-sm">{professor.department}</span>
-					<span class="flex items-center p-[10px] text-sm">
-						{(
-							professor.answers_copy
-								.map((item) => item.percentage)
-								.reduce((acc, curr) => acc + curr) / professor.answers_copy.length
-						).toFixed(0)} %
-					</span>
-				</div>
-
-				<!--Render for mobile view-->
-				<div class="md:hidden">
-					<div class="relative flex flex-col rounded-lg bg-slate-100 p-[1.25rem]">
-						<div class="leading-7">
-							<p>Teacher: Faculty Text {index + 1}</p>
-							<p>Subject: COMSCI 1110</p>
-							<p>Result: Satisfied</p>
-						</div>
-					</div>
-				</div>
-			{/each}
-		</div>
+		<Table.Root>
+			<Table.Header>
+				<Table.Row>
+					<Table.Head class="w-[3rem]"></Table.Head>
+					<Table.Head class="truncate">Teacher Name</Table.Head>
+					<Table.Head class="truncate">Department</Table.Head>
+					<Table.Head class="truncate">Result for you(%)</Table.Head>
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>
+				{#each dashboardRoute.getEvalds() ?? [] as professor, index}
+					<Table.Row>
+						<Table.Cell>
+							<Actions {professor} />
+						</Table.Cell>
+						<Table.Cell class="truncate font-medium">{professor.fullname}</Table.Cell>
+						<Table.Cell class="truncate">{professor.department}</Table.Cell>
+						<Table.Cell class="w-[7rem] truncate text-center">
+							{(
+								professor.answers_copy
+									.map((item) => item.percentage)
+									.reduce((acc, curr) => acc + curr) / professor.answers_copy.length
+							).toFixed(0)} %
+						</Table.Cell>
+					</Table.Row>
+				{/each}
+			</Table.Body>
+		</Table.Root>
 	</div>
 </div>
