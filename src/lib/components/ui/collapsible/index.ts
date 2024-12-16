93434e73f15f51d07a6cb697b8c17c32c916b1e4
Mike John Eviota <eviotamikejohnb@gmail.com>
2024-12-16 19:11:43 +0800

    bump version

--- src/lib/components/ui/collapsible/index.ts
+++ src/lib/components/ui/collapsible/index.ts
@@ -1,15 +1,15 @@
-import { Collapsible as CollapsiblePrimitive } from 'bits-ui';
-import Content from './collapsible-content.svelte';
+import { Collapsible as CollapsiblePrimitive } from "bits-ui";
 
-const Root = CollapsiblePrimitive.Root;
-const Trigger = CollapsiblePrimitive.Trigger;
+const Root: typeof CollapsiblePrimitive.Root = CollapsiblePrimitive.Root;
+const Trigger: typeof CollapsiblePrimitive.Trigger = CollapsiblePrimitive.Trigger;
+const Content: typeof CollapsiblePrimitive.Content = CollapsiblePrimitive.Content;
 
 export {
-  Root,
-  Content,
-  Trigger,
-  //
-  Root as Collapsible,
-  Content as CollapsibleContent,
-  Trigger as CollapsibleTrigger
+	Root,
+	Content,
+	Trigger,
+	//
+	Root as Collapsible,
+	Content as CollapsibleContent,
+	Trigger as CollapsibleTrigger,
 };
--- src/lib/components/ui/radio-group/radio-group-item.svelte
+++ src/lib/components/ui/radio-group/radio-group-item.svelte
@@ -1,28 +1,30 @@
 <script lang="ts">
-  import { RadioGroup as RadioGroupPrimitive } from 'bits-ui';
-  import Circle from 'lucide-svelte/icons/circle';
-  import { cn } from '$lib/utils.js';
+	import { RadioGroup as RadioGroupPrimitive, type WithoutChildrenOrChild } from "bits-ui";
+	import Circle from "lucide-svelte/icons/circle";
+	import { cn } from "$lib/utils.js";
 
-  type $$Props = RadioGroupPrimitive.ItemProps;
-  type $$Events = RadioGroupPrimitive.ItemEvents;
-
-  let className: $$Props['class'] = undefined;
-  export let value: $$Props['value'];
-  export { className as class };
+	let {
+		ref = $bindable(null),
+		class: className,
+		...restProps
+	}: WithoutChildrenOrChild<RadioGroupPrimitive.ItemProps> & {
+		value: string;
+	} = $props();
 </script>
 
 <RadioGroupPrimitive.Item
-  {value}
-  class={cn(
-    'aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-    className
-  )}
-  {...$$restProps}
-  on:click
+	bind:ref
+	class={cn(
+		"border-primary text-primary focus-visible:ring-ring aspect-square size-4 rounded-full border shadow focus:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
+		className
+	)}
+	{...restProps}
 >
-  <div class="flex items-center justify-center">
-    <RadioGroupPrimitive.ItemIndicator>
-      <Circle class="h-2.5 w-2.5 fill-current text-current" />
-    </RadioGroupPrimitive.ItemIndicator>
-  </div>
+	{#snippet children({ checked })}
+		<div class="flex items-center justify-center">
+			{#if checked}
+				<Circle class="fill-primary size-3.5" />
+			{/if}
+		</div>
+	{/snippet}
 </RadioGroupPrimitive.Item>
